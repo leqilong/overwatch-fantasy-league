f1f18e65c8497e31693f5d2007889695cca21e05
Leqi Long <leqilong@Leqis-MacBook-Pro.local>
2019-07-24 19:23:26 -0700

    Added team logos and match times

--- frontend/src/components/Predictions/MatchesList.js
+++ frontend/src/components/Predictions/MatchesList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import {fetchMatches, fetchPredictions} from '../../actions/PredictionsActions';
+import './MatchesList.scss'
 
 class MatchesList extends React.Component{
 
@@ -28,19 +29,30 @@ class MatchesList extends React.Component{
     }
   }
   renderMatchesList(){
-    // console.log('MatchesList component: ');
-    // console.log(this.props.matches);
     return this.props.matches.map(match =>{
       return(
         <div className="item" key={match.id}>
           {this.renderButton(match)}
           <div className="content">
-            <div className="item">
-              <p>{match['competitors'][0]['name']}</p>
+            <div className="team1Container">
+              <div className="item">
+                <p>{match['competitors'][0]['name']}</p>
+              </div>
+              <div className="item">
+                <img src={match['competitors'][0]['logo']} />
+              </div>
             </div>
             <div className="item">
-              <p>{match['competitors'][1]['name']}</p>
+              <p>{match['startDate']}</p>
             </div>
+            <div className="team2Container">
+              <div className="item">
+                <p>{match['competitors'][1]['name']}</p>
+              </div>
+              <div className="item">
+                <img src={match['competitors'][1]['logo']} />
+              </div>
+            </div>  
           </div>
         </div>
       )
