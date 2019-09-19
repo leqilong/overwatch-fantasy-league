d3875735cb8e485c3703b1476ad93a2e1906e73f
Leqi Long <leqilong@Leqis-MacBook-Pro.local>
2019-09-18 17:30:04 -0700

    Change api url

--- frontend/src/apis/predictions.js
+++ frontend/src/apis/predictions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: '',
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
